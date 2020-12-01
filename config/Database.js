d9a69e4c120dae8687600b2bffae27d34fa6c44a
Akram Ouardas <a.ouardas@esi-sba.dz>
2020-12-01 01:19:13 +0100

    deploying

--- config/Database.js
+++ config/Database.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const env ="development";
+const env = "development";
 // extracting database configuration from config.json
 const config = require("./config.json")[env];
 
